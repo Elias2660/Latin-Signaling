9468f20ab7f520e8d9c6bd999c4122576e4338bb
Elias Xu <124101026+Elias2660@users.noreply.github.com>
2024-08-09 22:04:56 -0400

    add some new files and schema

---
+++ src/models/Room.ts
@@ -0,0 +1,25 @@
+import { Schema, model, models } from "mongoose";
+
+const RoomSchema = new Schema({
+  name: {
+    type: String,
+    unique: [true, "Name already exists"],
+    required: [true, "Please provide a name"],
+  },
+  login_code: {
+    type: String,
+    required: [true, "Please provide a login code"],
+    unique: [true, "Login code already exists"],
+  },
+  teams: {
+    type: [String],
+    default: [],
+  },
+  admin: {
+    type: [String],
+    required: [true, "Please provide an admin"],
+  },
+});
+
+const Room = models.Room || model("Room", RoomSchema);
+export default Room;
